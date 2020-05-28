ee0ca04675ffabf804cd1dd69377608b8d89cfab
Paweł Kubiak <pawel.kubiak@protonmail.com>
2020-05-28 09:25:45 +0200

    feat: Add self contained fonts

---
+++ engine/drawing/text.py
@@ -0,0 +1,82 @@
+from typing import Tuple
+from ..font import Font
+from ..rt.image import Color4f, Image
+
+def blend(background: Color4f, foreground: Color4f) -> Color4f:
+    if foreground.a == 1.0:
+        return foreground
+    return (foreground.a * foreground) + (1 - foreground.a) * background
+
+
+def getbbox(text: str, font: Font) -> Tuple[int, int]:
+    pass
+
+def puttextblock(image: Image, x0, y0, line_width, text, *, font_color, font, shadow_color=None, align='left'):
+    pos = start = width = 0
+    text += '\n'
+    while pos < len(text):
+        if text[pos] == ' ':
+            new_width = width + 2
+        elif text[pos] != '\n':
+            new_width = width + font.glyphs[text[pos]].width + font.char_spacing
+        
+        if (text[pos] == '\n') or (new_width >= line_width):
+            if align=='left':
+                offset=x0
+            elif align=='right':
+                offset=x0+line_width-width
+            elif align=='center':
+                offset=x0 +(line_width -width)//2
+            puttext(image, offset, y0, text[start: pos], font_color=font_color, font=font, shadow_color=shadow_color)
+            y0 += font.line_height 
+            start = pos if text[pos] != '\n' else pos + 1
+            width = (new_width - width)
+        else:
+            width = new_width
+        
+        pos += 1
+
+
+def puttext(image: Image, x0, y0, text, *, font_color, font: Font, shadow_color = None):
+    # if font.full_color:
+    #     raise NotImplementedError('Full Color fonts are not supported')
+    if isinstance(font_color, tuple):
+        font_color = Color4f.from_tuple(font_color)
+    if isinstance(shadow_color, tuple):
+        shadow_color = Color4f.from_tuple(shadow_color)
+
+    offset_x = 0
+    offset_y = 0
+    for char in text:
+        if char == '\n':
+            offset_x = 0
+            offset_y += font.line_height
+            continue
+        if char == ' ':
+            offset_x += 2
+            continue
+        glyph = font.glyphs[char]
+
+        for y in range(glyph.height):
+            for x in range(glyph.width):
+                xx, yy = x0+offset_x+x, y0+offset_y+y
+                if not (0<=xx<image.width and 0<=yy<image.height):
+                    continue
+                orig_color = image[xx, yy]
+                color = glyph[x, y]
+                if color.a == 0.0:
+                    continue
+
+                if color == font.shadow:
+                    if shadow_color is None:
+                        continue
+                    color = shadow_color
+                    # color = blend(orig_color, shadow_color)
+                elif color == font.foreground:
+                    color = font_color
+                #     # color = blend(orig_color, font_color)
+                else:
+                    raise ValueError(f"{color}")
+                image[x0 + offset_x + x, y0 + offset_y + y] = color
+
+        offset_x += glyph.width + font.char_spacing
--- engine/font.py
+++ engine/font.py
@@ -1,27 +1,50 @@
-from rt.image import Image, Color3i, Color4f
+"""
+Module for handling Bitmap Fonts stored in PNM files.
+
+TBA
+
+"""
+import re
 from typing import List, Optional
+from .rt.image import Image, Color3i, Color4f
+
 
 
 class Font:
-    full_color = False
+    COLOR_REGEX = re.compile(r'\((\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)')
 
-    def __init__(self, path: str, foreground: Color3i, transparency: Color3i, glyphs: List[str], line_height: int, shadow: Optional[Color3i] = None, char_spacing: int = 1,):
-        self.image = Image.load(path, transparency=transparency)
-        self.glyphs = {}
+    @classmethod
+    def _parse_color(self, text: str):
+        return tuple(map(int, self.COLOR_REGEX.fullmatch(text).groups()))
+
+    def __init__(self, path: str): #, foreground: Color3i, transparency: Color3i, glyphs: List[str], line_height: int, shadow: Optional[Color3i] = None, char_spacing: int = 1,):
+        image = Image.load(path)
+
+        assert 'Transparency' in image.meta
+        transparency = Color4f.from_hex(image.meta['Transparency'])
+        image = Image.load(path, transparency=transparency.as_3i)
+
+        limit = 0
+        while f"Glyphs_{limit}" in image.meta: limit += 1
+        glyphs = [image.meta[f"Glyphs_{i}"] for i in range(limit)]
+
+        self.glyphs = self._detect_glyphs(image, glyphs)
+        # self.glyphs = {}
         
-        self.foreground = Color4f(foreground[0]/255, foreground[1]/255, foreground[2]/255, 1.0)
-        self.shadow = Color4f(shadow[0]/255, shadow[1]/255, shadow[2]/255, 1.0) if shadow else None
+        self.foreground = Color4f.from_hex(image.meta['Foreground'])
+        print(self.foreground)
+        self.shadow = Color4f.from_hex(image.meta['Shadow']) if 'Shadow' in image.meta else None 
+        
+        print(self.shadow)
+        self.char_spacing = int(image.meta.get('CharSpacing', 0))
+        self.line_height = int(image.meta.get('LineHeight', 0))
 
-        self.char_spacing = char_spacing
-        self.line_height = line_height
-
-        self._detect_glyphs(glyphs)
-
-    def _detect_glyphs(self, glyphs):
+    @classmethod
+    def _detect_glyphs(cls, image, glyphs):
         is_empty = []
-        for y in range(self.image.height):
+        for y in range(image.height):
         
-            is_empty.append(all(self.image[i, y].a == 0 for i in range(self.image.width)))
+            is_empty.append(all(image[i, y].a == 0 for i in range(image.width)))
 
         is_empty.append(True) # add guardian
 
@@ -36,15 +59,15 @@ class Font:
 
         assert len(lines) == len(glyphs)
 
-        
+        chars = {}
         for lineno, line in enumerate(lines):
             line_glyphs = []
             is_empty = []
             glyph_start = None
-            for x in range(self.image.width):
-                is_empty.append(all(self.image[x, i].a == 0 for i in range(line[0], line[1]+1)))
+            for x in range(image.width):
+                is_empty.append(all(image[x, i].a == 0 for i in range(line[0], line[1]+1)))
             is_empty.append(True)
-            
+
             for x in range(len(is_empty)):
                 if glyph_start is None and not is_empty[x]:
                     glyph_start = x
@@ -52,26 +75,28 @@ class Font:
                     line_glyphs.append((glyph_start, x-1))
                     glyph_start = None
             
-            assert len(line_glyphs) == len(glyphs[lineno]), f"Line #{lineno} expected to has {len(line_glyphs)} glyphs, but it has {len(glyphs[lineno])}"
+            assert len(line_glyphs) == len(glyphs[lineno]), f"Line #{lineno} expected to has {len(glyphs[lineno])} glyphs, but it has {len(line_glyphs)}"
 
             for c, pos in zip(glyphs[lineno], line_glyphs):
-                self.glyphs[c] = self.image.crop((pos[0], line[0], pos[1], line[1]))
+                chars[c] = image.crop((pos[0], line[0], pos[1], line[1]))
+
+        return chars
 
 # NOTE: https://opengameart.org/content/boxy-bold-font-0
-BOXY_BOLD_FONT_PLUS = Font('gfx/font.pnm', foreground=(255,255,255), shadow=(0,0,0), transparency=(0, 255,255), glyphs=[
-    '!"#$%&\'()*+,-./',
-    '0123456789',
-    ':;<=>?@',
-    'ABCDEFGHIJKLM',
-    'NOPQRSTUVWXYZ',
-    '[\\]^_`{|}~',
-    'abcdefghijklmn',
-    'opqrstuvwxyz'
-], char_spacing=-1, line_height=8)
+# BOXY_BOLD_FONT_PLUS = Font('gfx/font.pnm', foreground=(255,255,255), shadow=(0,0,0), transparency=(0, 255,255), glyphs=[
+#     '!"#$%&\'()*+,-./',
+#     '0123456789',
+#     ':;<=>?@',
+#     'ABCDEFGHIJKLM',
+#     'NOPQRSTUVWXYZ',
+#     '[\\]^_`{|}~',
+#     'abcdefghijklmn',
+#     'opqrstuvwxyz'
+# ], char_spacing=-1, line_height=8)
 
 
-MAGIC_FONT = Font('gfx/magic_font.pnm', foreground=(0, 0, 0), transparency=(0, 255,255), glyphs=[
-    '.ABCDEFGHIJKLMNO',
-    'PQRSTUVWXYZ()',
-    '-abcdefghijklmnopqrstuvwxyz<>',
-], line_height=12)
+# MAGIC_FONT = Font('gfx/magic_font.pnm', foreground=(0, 0, 0), transparency=(0, 255,255), glyphs=[
+#     '.ABCDEFGHIJKLMNO',
+#     'PQRSTUVWXYZ()',
+#     '-abcdefghijklmnopqrstuvwxyz<>',
+# ], line_height=12)
